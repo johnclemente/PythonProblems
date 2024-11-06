fcaec5acafa19130adc03e3b03378b60da2cefd4
John Clemente <john.clemente@focus.org>
2024-11-06 11:48:53 -0500

    update old input str

--- game/Number Guessing Project/main.py
+++ game/Number Guessing Project/main.py
@@ -2,7 +2,7 @@ import numberGuess
 
 def main():
     print(f"Welcome to the number guessing game.")
-    print("I am thinking of a number between 1-100, you have x tries to guess it")
+    print("I am thinking of a number between 1-100")
     numberGuess.numberGuess()
     
 if __name__ == '__main__':
