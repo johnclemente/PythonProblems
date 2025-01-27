a0477e86a0309e3dffbbd8f4f8787a1a7dffb9a6
John Clemente <john.clemente@focus.org>
2025-01-27 09:54:59 -0500

    add turtle challenge

---
+++ misc/turtleChallenges.py
@@ -0,0 +1,34 @@
+import turtle as t
+
+pointer = t.Turtle()
+
+# square
+# =========
+
+# for i in range(4):
+#     pointer.forward(100)
+#     pointer.right(90)
+ 
+# Dashed line
+# ============
+   
+# for i in range(100):
+#     pointer.penup()
+#     pointer.forward(10)
+#     pointer.pendown()
+#     pointer.forward(10)
+
+# Overlapping shapes
+# =====================
+
+# shapes = [4,5,6,7,8]
+# colors = ["blue", "red", "green", "yellow", "pink"]
+# for x in range(len(shapes)):
+#     pointer.pen(fillcolor=colors[x], pencolor=colors[x])
+#     for i in range(shapes[x]):
+#         pointer.forward(100)
+#         pointer.right(360/shapes[x])
+
+# random walk
+# ============
+
